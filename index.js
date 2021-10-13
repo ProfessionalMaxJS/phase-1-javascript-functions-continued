2b55b3339fd9db8773a353f6f1a6cf5a91a6eda7
Max <max.edison@yahoo.com>
2021-10-13 13:51:57 -0400

    commit tree 889ee12a6efe2502da09a77f858941b1b53fcf2c

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+function saturdayFun(soundsFun){
+if (soundsFun === undefined){
+    soundsFun = "roller-skate"
+}
+    return "This Saturday, I want to " + soundsFun + "!";
+}
+// defines saturdayFun function declaration as specified + uses the default activity 'roller-skate' when no arguments are passed + permits the default activity to be overriden
+
+const mondayWork = function(brightIdea){
+    if (brightIdea===undefined){
+        brightIdea = "go to the office"
+    }
+    return "This Monday, I will " + brightIdea + ".";
+}
+// defines mondayWork function expression as specified + uses the default activity 'go to the office' when no arguments are passed + permits the default activity to be overriden
+
+function wrapAdjective(flair){
+    if (flair===undefined){
+        flair="*";}
+            return function(special){
+            if (special===undefined){
+                special = "special";
+                }
+        return `You are ${flair}${special}${flair}!`
+            }
+     
+        }
+    
+    
